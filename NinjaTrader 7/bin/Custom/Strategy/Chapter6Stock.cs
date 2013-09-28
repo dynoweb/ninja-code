af1df97c2fda95a7bb51ab972810e283b5bc8d61
rcromer@avioconsulting.com <rcromer@avioconsulting.com@03469390-2f03-03c3-3189-c02ba7953020>
2013-09-28 22:30:04 +0000

    I believe this version matches the rules laid out like chapter 6 in the book, but my results are not as good.

--- NinjaTrader 7/bin/Custom/Strategy/Chapter6Stock.cs
+++ NinjaTrader 7/bin/Custom/Strategy/Chapter6Stock.cs
@@ -19,6 +19,17 @@ namespace NinjaTrader.Strategy
 	/// Written by Rick Cromer
 	/// Stock Strategy based on Chapter 6 of the book Building Reliable Trading Systems
 	/// Trades daily time period stocks
+	/// 
+	/// 9/28/2013 - For some reason, my numbers don't match the books.  I do more trades and am not making as much profit.
+	/// 
+	/// Test - Using 9/30/99 - 12/31/2011 12 years to allow indicators to get historical data
+	/// 9/28/13 
+	/// Conditions: dP=4, eAXB=9, lTP=40, lO=true, lVT=3, pT=0
+	/// Results: PF=1.57, Net=532,920.23, Trades=6523, AvgTrade=81.70, MaxDD=3,268.91 - No Commission
+	/// 
+	/// Conditions: dP=8, eAXB=8, lTP=70, lO=true, lVT=3, pT=300
+	/// Results: PF=1.61, Net=309,836.31, Trades=4907, AvgTrade=63.14, MaxDD=1,839.72 - No Commission
+	/// 
     /// </summary>
     [Description("Trades daily time period stocks, based on Chapter 5 of the book Building Reliable Trading Systems. Note when running live make sure to set the the 'Stop & target submission' property to ByStrategyPosition")]
     public class Chapter6Stock : Strategy
@@ -32,6 +43,11 @@ namespace NinjaTrader.Strategy
 		private int lowVolatilityThreshold = 3;
         private double profitTarget = 300.00; // Default setting for ProfitTarget, based on a $5000 investment
         // User defined variables (add any user defined variables below)
+
+		private bool forceReversalShort = false;	// looks for a short signal before enabling the long signal
+		private IOrder entryLongOrder = null;
+		private IOrder closeReversalOrder = null;
+		private IOrder closeXBarsOrder = null;
         #endregion
 
         /// <summary>
@@ -51,6 +67,7 @@ namespace NinjaTrader.Strategy
 			if (profitTarget != 0) SetProfitTarget(profitTarget);
 			
 			Add(DonchianChannelClose(donchianPeriod));			
+			Print("Initializing Strategy");
         }
 
         /// <summary>
@@ -62,29 +79,41 @@ namespace NinjaTrader.Strategy
 			if (CurrentBar < LongerTermPeriod)
         		return;
 
+			// EXIT CODE			
 			if (Position.MarketPosition == MarketPosition.Long)
-			{					
-				if (ExitAfterXBars != 0 && BarsSinceEntry() >= ExitAfterXBars)
-					ExitLong();
+			{
+				// Exit after x bars
+				if (ExitAfterXBars != 0 
+					&& BarsSinceEntry() >= ExitAfterXBars
+					)
+				{
+					closeXBarsOrder = ExitLong("AfterXBars", "Reversal");
+				}
 				
+				// Exit if short reversal detected
 				if (ExitAfterXBars == 0 && DonchianPeriod != 0 
-					&& Close[0] > MAX(Close, DonchianPeriod)[1])
-						ExitLong();								
-			}
+					&& Close[0] > MAX(Close, DonchianPeriod)[1]
+					)
+				{
+					closeReversalOrder = ExitLong("Breakout", "Reversal");								
+				}
+			}			
 			
-			if (Position.MarketPosition == MarketPosition.Flat)
+			// ENTRY CODE
+			if (Position.MarketPosition != MarketPosition.Long)
 			{				
 				if (Close[0] < MIN(Close, DonchianPeriod)[1]  // using reverse entry signals
 					&& (LongerTermPeriod == 0 || Close[0] > Close[LongerTermPeriod])
 					&& (LowVolatilityThreshold == 0 || !lowVolatility(LowVolatilityThreshold))
 					)
 				{
-					EnterLong(calcShares(5000));
+					entryLongOrder = EnterLong(calcShares(5000), "Reversal");
 				}
-//				if (!LongOnly && Close[0] > MAX(Close, DonchianPeriod)[1])
-//				{
-//					EnterShort(calcShares(5000));
-//				}
+			}
+			
+			if (!LongOnly && Close[0] > MAX(Close, DonchianPeriod)[1])
+			{
+				EnterShort(calcShares(5000), "Reversal");
 			}
         }
 		
@@ -100,7 +129,7 @@ namespace NinjaTrader.Strategy
 		{
 			int Period = 20;
 			double stdDevValue = StdDev(Close, Period)[0];
-			Print (Time + " stdDev percent of price " + ((stdDevValue/Close[0]) * 100));
+			//Print (Time + " stdDev percent of price " + ((stdDevValue/Close[0]) * 100));
 			return (stdDevValue/Close[0]) * 100 < minPercent ? true : false;
 		}
 		
@@ -111,6 +140,38 @@ namespace NinjaTrader.Strategy
 //			return 1; 
 		}
 
+		// OnOrderUpdate() - Called when a strategy generated order changes state
+		protected override void OnOrderUpdate(IOrder order)
+		{
+			if (entryLongOrder != null && entryLongOrder == order)
+			{
+				if (order.OrderState == OrderState.Filled)
+				{
+					//Print(Time + " entryLongOrder: " + order.ToString());
+					//entryLongOrder = null;
+				}
+			}
+			
+//1/12/2009 3:00:00 PM entryLongOrder: Order='NT-00226/Backtest' Name='Reversal' State=Filled Instrument='WYNN' Action=Buy Limit price=0 Stop price=0 Quantity=121 Strategy='Chapter6Stock' Type=Market Tif=Gtc Oco='' Filled=121 Fill price=40.82 Token='2e871a63e35e43a5949fbbacec0256e4' Gtd='12/1/2099 12:00:00 AM'
+//1/26/2009 3:00:00 PM closeXBarsOrder: Order='NT-00228/Backtest' Name='AfterXBars' State=Filled Instrument='WYNN' Action=Sell Limit price=0 Stop price=0 Quantity=121 Strategy='Chapter6Stock' Type=Market Tif=Gtc Oco='' Filled=121 Fill price=35.76 Token='1746944728aa45c7b226538e6579655f' Gtd='12/1/2099 12:00:00 AM'
+			
+			if (closeXBarsOrder != null && closeXBarsOrder == order)
+			{
+				// normal State life cycle PendingSubmit, Accepted, Working, Filled
+				if (order.OrderState == OrderState.Filled)
+				{
+					Print(Time + " entryLongOrder: " + order.ToString());
+					Print(Time + " closeXBarsOrder: " + order.ToString());
+					double profit = entryLongOrder.Quantity * (closeXBarsOrder.AvgFillPrice - entryLongOrder.AvgFillPrice);
+					Print("Profit: " + profit); 
+					//closeXBarsOrder = null;
+					if (profit < 0)
+						forceReversalShort = true;
+				}
+			}
+		}
+ 
+		// OnExecution() - Called when a strategy generated order is filled
 		protected override void OnExecution(IExecution execution)
 		{
 			// Remember to check the underlying IOrder object for null before trying to access its properties
@@ -118,8 +179,9 @@ namespace NinjaTrader.Strategy
 			{
 				if (execution.Order.OrderAction == OrderAction.Buy)
 				{
-					//Print(Time + " OnExecution " + execution.ToString());
-					//Print(Time + " OnExecution     " + execution.Order.ToString());
+//					Print(Time + " OnExecution Buy " + execution.ToString());
+//					Print(Time + " OnExecution Buy     " + execution.Order.ToString());
+//					Print(Time + " " + Position.Quantity + "@" + Position.AvgPrice);
 					if (ProfitTarget != 0)
 					{
 						//double profitPerShare = profitTarget/execution.Order.Quantity;
@@ -133,6 +195,13 @@ namespace NinjaTrader.Strategy
 					// Reset profit target to never hit value
 					//if (Position.MarketPosition == MarketPosition.Flat)
 					//	SetProfitTarget(99999999);
+					
+//1/26/2009 3:00:00 PM OnExecution Execution='NT-00155' Instrument='WYNN' Account='Backtest' Name='Sell' Exchange=Default Price=35.76 Quantity=121 Market position=Short Commission=9.99 Order='NT-00228' Time='1/27/2009 3:00:00 PM'
+//1/26/2009 3:00:00 PM OnExecution     Order='NT-00228/Backtest' Name='Sell' State=Filled Instrument='WYNN' Action=Sell Limit price=0 Stop price=0 Quantity=121 Strategy='Chapter6Stock' Type=Market Tif=Gtc Oco='' Filled=121 Fill price=35.76 Token='e8c28c3cabef43939b891c67b0c110ad' Gtd='12/1/2099 12:00:00 AM'
+					
+//					Print(Time + " OnExecution " + execution.ToString());
+//					Print(Time + " OnExecution     " + execution.Order.ToString());
+//					Print(Time + " Action=" + execution.Order.OrderAction + "  " + execution.Order.Quantity + "@" + execution.Order.AvgFillPrice + " Opened @ " + Position.AvgPrice);
 				}
 			} 
 		}
